0536261da6204a0be6cb2cc3848997cfcf2eedef
SaharWeiss <sahar7weiss@gmail.com>
2023-09-09 14:37:08 +0300

    final p14

--- app/src/main/java/il/co/sahar/blogs/ACTIVITIES/BlogPostActivity.java
+++ app/src/main/java/il/co/sahar/blogs/ACTIVITIES/BlogPostActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -15,27 +16,12 @@ import java.util.Locale;
 import il.co.sahar.blogs.R;
 
 public class BlogPostActivity extends AppCompatActivity {
-    final Calendar myCalendar= Calendar.getInstance();
-    EditText DateText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_post);
-        DateText=(EditText) findViewById(R.id.Dateid);
-        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
-            @Override
 
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, month);
-                myCalendar.set(Calendar.DAY_OF_MONTH, day);
-                updateLabel();
-            }
-        };
+
     }
-    private void updateLabel(){
-        String myFormat="DD/mm/yy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.UK);
-        DateText.setText(dateFormat.format(myCalendar.getTime()));
-    }
+
 }
