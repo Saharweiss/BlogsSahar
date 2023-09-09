c88fba2028f70c769f794f95021a9be38d9b9e84
SaharWeiss <sahar7weiss@gmail.com>
2023-09-09 14:21:58 +0300

    almost done

--- app/src/main/java/il/co/sahar/blogs/ACTIVITIES/MainActivity.java
+++ app/src/main/java/il/co/sahar/blogs/ACTIVITIES/MainActivity.java
@@ -2,7 +2,9 @@ package il.co.sahar.blogs.ACTIVITIES;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import il.co.sahar.blogs.R;
@@ -16,7 +18,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initializeViews();
     }
 
@@ -29,6 +30,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setListeners() {
-        // Add listener to btnAddPost
+        //no need
     }
+    public void addPost(View view)
+    {
+        Intent intent=new Intent(this,BlogPostActivity.class);
+        startActivity(intent);
+    }
+
 }
